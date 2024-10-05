7611452d03069758be0dcad766629a222c010e4d
Aditya Raj Singh <thisis.adityarajsingh@gmail.com>
2024-10-05 10:00:00 +0530

    feat: initialize bitcoin crate and workspace setup

---
+++ bitcoin/src/consensus/mod.rs
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: CC0-1.0
+
+//! Bitcoin consensus.
+//!
+//! This module defines structures, functions, and traits that are needed to
+//! conform to Bitcoin consensus.
+//!
+
+pub mod encode;
+pub mod params;
+
+pub use self::encode::{
+    deserialize, deserialize_partial, serialize, Decodable, Encodable, ReadExt, WriteExt,
+};
+pub use self::params::Params;
+
+#[cfg(feature = "serde")]
+#[cfg_attr(docsrs, doc(cfg(feature = "serde")))]
+pub mod serde;
